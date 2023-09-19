27420b1684a948489a0bfb1f89e1e606c6eba6c1
ruisinhofilipe <ruisinhofilipe@gmail.com>
2023-09-19 16:33:14 +0200

    user model

---
+++ server/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  nickname: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
