eb638fe816dcd62bdc19a171ab52e95724f2a28b
ruisinhofilipe <ruisinhofilipe@gmail.com>
2023-09-20 15:28:29 +0200

    Fixed conflits

--- server/routes/games.js
+++ server/routes/games.js
@@ -1,24 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const saveGame = require('../models/games.model');
+const mongoose = require("mongoose");
+const saveGame = require("../models/games.model");
 
-router.post('/api/submit', (req, res) => {
-    const { name, price, stock } = req.body;
+router.post("/api/submit", (req, res) => {
+  const { name, price, stock } = req.body;
 
-    const games = new saveGame({
-        name,
-        price,
-        stock
+  const games = new saveGame({
+    name,
+    price,
+    stock,
+  });
+
+  games
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "New game saved." });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          message: "There's been an error while trying to save the new game",
+        });
     });
-
-    games.save()
-        .then(() => {
-            res.status(200).json({ message: 'Jeu enregistré avec succès' });
-        })
-        .catch((err) => {
-            res.status(500).json({ message: 'Erreur lors de l\'enregistrement du jeu' });
-        })
 });
 
 module.exports = router;
