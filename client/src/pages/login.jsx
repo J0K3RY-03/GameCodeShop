81b8a5cbca599879e65a4fd135b05dc5f05ec39e
ruisinhofilipe <ruisinhofilipe@gmail.com>
2023-09-22 14:31:20 +0200

    added scss variables + login inputs style

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
@@ -31,28 +32,38 @@ export const Login = () => {
   };
 
   return (
-    <div className="login-form">
-      <form onSubmit={onSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="string"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
+    <div className="login-container">
+      <div className="login-container-left">
+        <form className="login-form" onSubmit={onSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
+          <input
+            type="password"
+            placeholder="**********"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
 
-        <button type="submit">Login</button>
-      </form>
+          <button type="submit" className="login-button">
+            Submit
+          </button>
+        </form>
+        <Link to="/register" className="login-registerLink">
+          No account yet ?
+        </Link>
+      </div>
+      <div className="login-container-right">
+        <p>future image</p>
+      </div>
     </div>
   );
 };
