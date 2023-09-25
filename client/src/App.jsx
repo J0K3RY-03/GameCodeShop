ec397d7bbcca9b21ee082893970c738a2d679a76
ruisinhofilipe <ruisinhofilipe@gmail.com>
2023-09-25 13:14:36 +0200

    Login page doesn't display the Header comp anymore, possible to be redirected to Register or Home from Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import GameForm from "./pages/newGame.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
@@ -8,18 +8,25 @@ import { Register } from "./pages/register";
 import { Home } from "./pages/home";
 
 function App() {
+  // // Current location
+  const location = useLocation();
+
+  // Array that contains all the paths where I don't want to display the Header
+  const pathsWithoutHeader = ["/login"];
+
+  // Check if the array contains the path
+  const shouldDisplayHeader = !pathsWithoutHeader.includes(location.pathname);
+
   return (
     <div className="App">
-      <Router>
-        <Header></Header>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/new-game" element={<GameForm />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-        </Routes>
-        <Footer></Footer>
-      </Router>
+      {shouldDisplayHeader ? <Header /> : null}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/new-game" element={<GameForm />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
