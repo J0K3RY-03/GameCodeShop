28c02cb6c4e382538616fe8654a69f6469f0c30a
ruisinhofilipe <ruisinhofilipe@gmail.com>
2023-09-26 14:53:57 +0200

    user now can login and logout - the cookies will be wiped out when logout is pressed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,31 @@
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userID");
+    navigate("/");
+  };
+
   return (
     <div className="navbar">
       <Link to="/">Home</Link>
       <Link to="/new-game">Add new game</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+
+      {cookies.access_token ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </>
+      )}
     </div>
   );
 };
